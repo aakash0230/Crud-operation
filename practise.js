1e680566ae1a44d751582aeb5ea3fbeeaa3b79b3
Aakash Pandey <pandeyaakash123.ap@gmail.com>
2023-06-06 23:54:26 +0530

    Updated

---
+++ practise.js
@@ -0,0 +1,17 @@
+// arr = [[0, 1], [2, 3], [4, 5]];
+
+// arrResult = []
+
+// for(let i = 0; i < 3; i++){
+//     for(let j = 0; j < 2; j++){
+//         arrResult.push(arr[i][j]);
+//         // console.log(arr[i][j]);
+//     }
+// }
+
+// console.log(arrResult);
+
+console.log(typeof undefined);
+console.log(typeof NULL)
+console.log(typeof null)
+console.log(typeof typeof 1);
